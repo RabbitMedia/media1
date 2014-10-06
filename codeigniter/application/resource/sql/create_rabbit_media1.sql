608ff161283e8be86874ede0c8dd5b74129a0b76
RabbitMedia <inquiry.rabbit@gmail.com>
2014-10-06 21:29:31 +0900

    テーブル定義変更

--- codeigniter/application/resource/sql/create_rabbit_media1.sql
+++ codeigniter/application/resource/sql/create_rabbit_media1.sql
@@ -16,27 +16,22 @@ CREATE TABLE `video_master` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT '動画マスター情報';
 
 CREATE TABLE `video_id` (
-  `id`           INT(10)    UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'ID',
   `master_id`    INT(10)    UNSIGNED NOT NULL                COMMENT '動画マスターID',
   `type`         TINYINT(3) UNSIGNED NOT NULL                COMMENT 'タイプ',
   `video_url_id` VARCHAR(128)        NOT NULL                COMMENT '動画URLID',
   `create_time`  DATETIME            NOT NULL                COMMENT '作成日時',
   `update_time`  DATETIME            NOT NULL                COMMENT '更新日時',
   `delete_time`  DATETIME            DEFAULT NULL            COMMENT '削除日時',
-  PRIMARY KEY (`id`),
-  KEY `video_id_idx_01` (`master_id`),
-  KEY `video_id_idx_02` (`type`,`video_url_id`)
+  KEY `video_id_idx_01` (`master_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT '動画ID情報';
 
 CREATE TABLE `video_category` (
-  `id`           INT(10)    UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'ID',
   `master_id`    INT(10)    UNSIGNED NOT NULL                COMMENT '動画マスターID',
   `category`     TINYINT(3) UNSIGNED NOT NULL                COMMENT 'カテゴリー',
   `display_flag` TINYINT(1) UNSIGNED NOT NULL DEFAULT '0'    COMMENT '表示フラグ',
   `create_time`  DATETIME            NOT NULL                COMMENT '作成日時',
   `update_time`  DATETIME            NOT NULL                COMMENT '更新日時',
   `delete_time`  DATETIME            DEFAULT NULL            COMMENT '削除日時',
-  PRIMARY KEY (`id`),
   KEY `video_category_idx_01` (`master_id`,`display_flag`),
   KEY `video_category_idx_02` (`category`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT '動画カテゴリー情報';
@@ -51,27 +46,23 @@ CREATE TABLE `crawler_video_master` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT 'クローラー動画マスター情報';
 
 CREATE TABLE `crawler_video_id` (
-  `id`                INT(10)    UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'ID',
   `crawler_master_id` INT(10)    UNSIGNED NOT NULL                COMMENT 'クローラー動画マスターID',
   `type`              TINYINT(3) UNSIGNED NOT NULL                COMMENT 'タイプ',
   `video_url_id`      VARCHAR(128) 	      NOT NULL                COMMENT '動画URLID',
   `create_time`       DATETIME            NOT NULL                COMMENT '作成日時',
   `update_time`       DATETIME            NOT NULL                COMMENT '更新日時',
   `delete_time`       DATETIME            DEFAULT NULL            COMMENT '削除日時',
-  PRIMARY KEY (`id`),
   KEY `crawler_video_id_idx_01` (`crawler_master_id`),
-  KEY `crawler_video_id_idx_02` (`type`,`video_url_id`)
+  UNIQUE `crawler_video_id_idx_02` (`type`,`video_url_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT 'クローラー動画ID情報';
 
 CREATE TABLE `crawler_video_title` (
-  `id`                INT(10)    UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'ID',
   `crawler_master_id` INT(10)    UNSIGNED NOT NULL                COMMENT 'クローラー動画マスターID',
   `media`             TINYINT(3) UNSIGNED DEFAULT NULL            COMMENT 'メディア',
   `title`             VARCHAR(128)        DEFAULT NULL            COMMENT 'タイトル',
   `create_time`       DATETIME            NOT NULL                COMMENT '作成日時',
   `update_time`       DATETIME            NOT NULL                COMMENT '更新日時',
   `delete_time`       DATETIME            DEFAULT NULL            COMMENT '削除日時',
-  PRIMARY KEY (`id`),
   KEY `crawler_video_title_idx_01` (`crawler_master_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT 'クローラー動画タイトル情報';
 
