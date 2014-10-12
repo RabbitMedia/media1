ef16a85370c33185872bceb382d0f610baecab72
RabbitMedia <inquiry.rabbit@gmail.com>
2014-10-13 04:12:11 +0900

    アップ待ち無しページ実装

--- codeigniter/application/views/admin/crawled_videos_empty.php
+++ codeigniter/application/views/admin/crawled_videos_empty.php
@@ -1,54 +1,77 @@
-<!DOCTYPE HTML>
-<html>
+<!DOCTYPE html>
+<html lang="ja">
 	<head>
-		<title>管理画面</title>
-		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
-		<!--[if lte IE 8]><script src="/css/ie/html5shiv.js"></script><![endif]-->
-		<script src="/js/jquery.min.js"></script>
-		<script src="/js/skel.min.js"></script>
-		<script src="/js/skel-layers.min.js"></script>
-		<script src="/js/init.js"></script>
-		<script src="/js/jquery.quicksearch.js"></script>
-		<noscript>
-			<link rel="stylesheet" href="/css/skel.css" />
-			<link rel="stylesheet" href="/css/style.css" />
-			<link rel="stylesheet" href="/css/style-desktop.css" />
-			<link rel="stylesheet" href="/css/style-wide.css" />
-		</noscript>
-		<!--[if lte IE 8]><link rel="stylesheet" href="/css/ie/v8.css" /><![endif]-->
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="copyright" content="&copy;セックル速報" />
+		<meta name="robots" content="NOINDEX,NOFOLLOW" />
+		<title>Dashboard</title>
+		<link rel="shortcut icon" type="image/x-icon" href="/image/favicon.ico" />
+		<link rel="icon" type="image/png" href="/image/favicon.png" />
+		<link rel="apple-touch-icon-precomposed" href="/image/apple-touch-icon-precomposed.png" />
+
+		<!-- Bootstrap -->
+		<link rel="stylesheet" href="/css/bootstrap.min.css">
+		<!-- Custom styles for Sticky footer with fixed navbar -->
+		<link rel="stylesheet" href="/css/sticky-footer-navbar.css">
+
+		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+			<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+			<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
 	</head>
-	<body class="left-sidebar">
+	<body>
 
-		<!-- Wrapper -->
-			<div id="wrapper">
+		<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+			<div class="container">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#mobile-menu">
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand" href="/admin">Dashboard</a>
+				</div>
 
-				<!-- Content -->
-					<div id="content">
-						<div class="inner">
+				<div class="collapse navbar-collapse" id="mobile-menu">
+					<ul class="nav navbar-nav">
+						<li class="active"><a href="/admin/crawled_videos">アップ待ち <span class="badge"><?=$total_count?></span></a></li>
+						<li><a href="/admin">アップ済み</a></li>
+					</ul>
+					<ul class="nav navbar-nav navbar-right">
+						<li><a href="/admin/logout">ログアウト</a></li>
+					</ul>
+				</div>
+			</div>
+		</nav>
 
-							<h1>アップ待ちの動画はありません</h1>
+		<div class="container">
 
-						</div>
-					</div>
+			<div class="row">
 
-				<!-- Sidebar -->
-					<div id="sidebar">
-					
-						<!-- Logo -->
-							<div id="logo"><a href="/admin">Dashboard</a></div>
-					
-						<!-- Nav -->
-							<nav id="nav">
-								<ul>
-									<li class="current"><a href="/admin/crawled_videos">アップ待ち（<?=$total_count?>）</a></li>
-									<li><a href="/admin">アップ済み</a></li>
-									<li><a href="/admin/logout">ログアウト</a></li>
-								</ul>
-							</nav>
-
-					</div>
+				<div class="col-xs-12 text-center">
+					<h1>アップ待ちの動画はありません</h1>
+				</div>
 
 			</div>
 
+		</div>
+
+		<div class="footer">
+			<div class="container text-center">
+				<p class="text-muted">Copyright &copy; <?php echo date("Y"); ?> 無料セックス動画まとめ【セックル速報】All Rights Reserved.</p>
+			</div>
+		</div>
+
+		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+		<script>window.jQuery || document.write('<script src="/js/jquery.min.js"><\/script>')</script>
+		<!-- Include all compiled plugins (below), or include individual files as needed -->
+		<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+		<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+		<script src="/js/ie10-viewport-bug-workaround.js"></script>
 	</body>
 </html>
--- codeigniter/application/views/admin/index.php
+++ codeigniter/application/views/admin/index.php
@@ -49,7 +49,7 @@
 						<span class="icon-bar"></span>
 						<span class="icon-bar"></span>
 					</button>
-					<a class="navbar-brand" href="#">Dashboard</a>
+					<a class="navbar-brand" href="/admin">Dashboard</a>
 				</div>
 
 				<div class="collapse navbar-collapse" id="mobile-menu">
